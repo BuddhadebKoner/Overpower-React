38fcd76e055bee9ab06b6d055f0e747c6d269d2d
buddhadebkoner <jeetkoner36@gmail.com>
2024-05-08 19:56:08 +0530

    chore: Update color change button event from onClick to onMouseEnter and adjust button size

--- Bg_Changer/src/App.jsx
+++ Bg_Changer/src/App.jsx
@@ -1,38 +1,49 @@
 import React, { useState, useEffect } from "react";
 import Buttons from "./components/Buttons";
 
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 function App() {
-  const [color, setColor] = useState("");
+    const [color, setColor] = useState("57a773");
+    const [colorsArray, setColorsArray] = useState([]);
 
-  useEffect(() => {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    setColor(randomColor);
-  }, []);
+    useEffect(() => {
+        const colors = [
+            "7ae582", "1a659e", "d62828", "540b0e", "00bbf9",
+            "2a9d8f", "9b5de5", "fca311", "52796f", "353535", "1c7293","cbf3f0","718355"
+        ];
+        setColorsArray(shuffleArray(colors));
+    }, []);
 
-  return (
-    <>
-      <div
-        className="Main-container w-full h-screen duration-500"
-        style={{ backgroundColor: `#${color}` }}
-      >
-        <div className="fixed flex flex-wrap justify-center bottom-20 inset-x-0 px-2">
-          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3 rounded-3xl">
-            <Buttons Color="7ae582" setColor={setColor} />
-            <Buttons Color="1a659e" setColor={setColor} />
-            <Buttons Color="d62828" setColor={setColor} />
-            <Buttons Color="540b0e" setColor={setColor} />
-            <Buttons Color="00bbf9" setColor={setColor} />
-            <Buttons Color="2a9d8f" setColor={setColor} />
-            <Buttons Color="9b5de5" setColor={setColor} />
-            <Buttons Color="fca311" setColor={setColor} />
-            <Buttons Color="52796f" setColor={setColor} />
-            <Buttons Color="353535" setColor={setColor} />
-            <Buttons Color="1c7293" setColor={setColor} />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+    const handleButtonClick = (color) => {
+        setColor(color);
+    };
+
+    
+
+    return (
+        <>
+            <div
+                className="Main-container w-full h-screen duration-500"
+                style={{ backgroundColor: `#${color}` }}
+            >
+                <div className="fixed flex flex-wrap justify-center bottom-20 inset-x-0 px-2">
+                    <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3 rounded-3xl">
+                        {colorsArray.map((color, index) => (
+                            <Buttons key={index} Color={color} setColor={handleButtonClick} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- Bg_Changer/src/components/Buttons.jsx
+++ Bg_Changer/src/components/Buttons.jsx
@@ -6,8 +6,8 @@ export default function Buttons({ Color, setColor }) {
     return (
         <>
             <button
-                onClick={() => setColor(Color)}
-                className="outline-none px-7 py-3 text-white rounded-full shadow-lg"
+                onMouseEnter={() => setColor(Color)}
+                className="outline-none h-10 w-20 px-7 py-3 text-white rounded-full shadow-lg"
                 style={{ backgroundColor: `#${Color}` }}
             >
             </button>
